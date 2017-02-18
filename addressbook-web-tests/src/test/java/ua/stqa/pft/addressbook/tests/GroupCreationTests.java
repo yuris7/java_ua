fab708410035d381c5c5f6fdf7fa9ffdf785e6de
yuris7 <yurisson7@gmail.com>
2017-02-18 23:48:32 +0200

    added Lambda comparing to Groutp Creation & GroupMOdification

--- addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -24,9 +25,11 @@ public class GroupCreationTests extends TestBase {
         max = g.getId();
       }
 
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before,after);
    }
 }
 
